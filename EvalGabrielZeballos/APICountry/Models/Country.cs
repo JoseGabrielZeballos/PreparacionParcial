46d21d37a5b4329110cd41379f9045648e61a4d5
JoseGabrielZeballos <a2019117789@estudiantes.upsa.edu.bo>
2021-03-13 11:37:43 -0400

    Modelo incorporado

---
+++ EvalGabrielZeballos/APICountry/Models/Country.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace APICountry.Models
+{
+    public class Country
+    {
+        [Key]
+        public string name { get; set; }
+        
+        [Required]
+        public string capital { get; set; }  
+        public int population { get; set; }
+        public List<double> latlng { get; set; }
+        public List<string> timezones { get; set; }
+        public List<Currency> currencies { get; set; }
+        public string flag { get; set; }
+        
+    }
+}
